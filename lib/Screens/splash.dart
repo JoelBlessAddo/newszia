6047cecaea88bb0977408390a9f1f89d9c5954a0
Browser283 <addojoel283@gmail.com>
2023-11-12 19:19:57 +0000

    started

---
+++ lib/Screens/splash.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class Splash extends StatefulWidget {
+  const Splash({super.key, required String title});
+
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
