ff1b8f4c33c529cbbf572ddbc9fc3943b20cf3e7
Browser283 <addojoel283@gmail.com>
2023-11-22 12:45:44 +0000

    progressing

--- lib/Screens/search.dart
+++ lib/Screens/search.dart
@@ -3,7 +3,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-
 class Search extends StatefulWidget {
   const Search({super.key});
 
@@ -21,9 +20,10 @@ class _SearchState extends State<Search> {
         iconTheme: IconThemeData(color: Colors.black),
         elevation: 0,
       ),
-      body: Column(children: [
-         Center(
-           child: Container(
+      body: Column(
+        children: [
+          Center(
+            child: Container(
               height: 60,
               width: 350,
               margin: EdgeInsets.symmetric(horizontal: 30),
@@ -31,32 +31,45 @@ class _SearchState extends State<Search> {
                 borderRadius: BorderRadius.all(Radius.circular(15)),
                 color: Color.fromARGB(255, 217, 221, 221),
               ),
-              child:Padding(
-              padding: const EdgeInsets.only(left: 10, right: 10, top: 10),
-              child: TextField(
-                decoration: InputDecoration(
-                  border: InputBorder.none,
-                  hintText: "Search news, source or category",
-                  prefixIcon: Icon(
-                    Icons.search_outlined,
-                    color: Colors.black,
-                  ),
-                  suffixIcon: Icon(
-                    Icons.equalizer_outlined,
-                    color: Colors.black,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 10, right: 10, top: 10),
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: "Search news, source or category",
+                    prefixIcon: Icon(
+                      Icons.search_outlined,
+                      color: Colors.black,
+                    ),
+                    suffixIcon: Icon(
+                      Icons.equalizer_outlined,
+                      color: Colors.black,
+                    ),
                   ),
                 ),
               ),
             ),
+          ),
+          SizedBox(height: 10),
+          Padding(
+            padding: const EdgeInsets.only(right: 200, top: 10),
+            child: Text(
+              "Recent search",
+              style:
+                  GoogleFonts.roboto(fontWeight: FontWeight.w800, fontSize: 16),
             ),
-         ),
-         SizedBox(height: 10),
-         Padding(
-           padding: const EdgeInsets.only(right: 200, top:10),
-           child: Text("Recent search", style: GoogleFonts.roboto(fontWeight: FontWeight.w800, fontSize: 16),),
-         ),
-        
-      ],),
+          ),
+          Row(
+            children: [
+              ListView.builder(
+                  itemCount: 4,
+                  itemBuilder: ((context, index) {
+                    return ListTile(leading: Text("data"));
+                  }))
+            ],
+          )
+        ],
+      ),
     );
   }
 }
--- lib/Tabs/all.dart
+++ lib/Tabs/all.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class All extends StatefulWidget {
   const All({super.key});
@@ -10,8 +11,137 @@ class All extends StatefulWidget {
 }
 
 class _AllState extends State<All> {
+  final List<Map<String, dynamic>> content = [
+    {
+      "text": "Peak oil is coming.That won't\nsave the world",
+      "image": "assets/gf.jpg",
+      "time": "1 min ago",
+      "reads": "12 min ago",
+      "ID": "Business"
+    },
+    {
+      "text": "Peak oil is coming. That won't save the world",
+      "image": "assets/fot1.jpeg",
+      "time": "1 min ago",
+      "reads": "12 min ago",
+      "ID": "Mining"
+    },
+    {
+      "text": "Peak oil is coming. That won't save the world",
+      "image": "assets/g1.png",
+      "time": "1 min ago",
+      "reads": "12 min ago",
+      "ID": "Local"
+    },
+    {
+      "text": "Peak oil is coming. That won't save the world",
+      "image": "assets/br1.jpeg",
+      "time": "1 min ago",
+      "reads": "12 min ago",
+      "ID": "Abroad"
+    },
+    {
+      "text": "Peak oil is coming. That won't save the world",
+      "image": "assets/fott.jpeg",
+      "time": "1 min ago",
+      "reads": "12 min ago",
+      "ID": "Africa"
+    }
+  ];
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    double h = MediaQuery.of(context).size.height;
+    double w = MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            for (var data in content)
+              Container(
+                margin: EdgeInsets.all(20),
+                height: h * 0.19,
+                width: w * 1,
+                decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 231, 227, 227),
+                    borderRadius: BorderRadius.all(Radius.circular(15))),
+                child: Stack(
+                  children: [
+                    Positioned(
+                        top: 10,
+                        left: 10,
+                        bottom: 10,
+                        right: 230,
+                        child: Container(
+                            decoration: BoxDecoration(
+                                color: Colors.redAccent,
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(15))),
+                            child: ClipRRect(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(15)),
+                                child: Image.asset(
+                                  data["image"],
+                                  fit: BoxFit.cover,
+                                )))),
+                    Positioned(
+                        top: 10,
+                        left: 130,
+                        right: 0,
+                        bottom: 0,
+                        child: Text(
+                          data['ID'],
+                          style: GoogleFonts.roboto(
+                              color: Colors.black, fontWeight: FontWeight.w500),
+                        )),
+                    Positioned(
+                        top: 10,
+                        left: 200,
+                        right: 0,
+                        bottom: 0,
+                        child: Text(data['reads'],
+                            style: GoogleFonts.roboto(
+                                color: Colors.black,
+                                fontWeight: FontWeight.w800))),
+                    Positioned(
+                        top: 0,
+                        left: 310,
+                        right: 10,
+                        bottom: 120,
+                        child: Container(
+                          decoration: BoxDecoration(
+                              color: Color.fromARGB(255, 231, 227, 227),
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(10))),
+                          child: IconButton(
+                            onPressed: () {},
+                            icon: Icon(Icons.bookmark_outline_outlined),
+                            color: Colors.redAccent,
+                          ),
+                        )),
+                    Positioned(
+                        top: 40,
+                        left: 130,
+                        right: 10,
+                        bottom: 0,
+                        child: Text(data['text'],
+                            style: GoogleFonts.roboto(
+                                color: Colors.black,
+                                fontWeight: FontWeight.w900))),
+                    Positioned(
+                        top: 100,
+                        left: 130,
+                        right: 10,
+                        bottom: 0,
+                        child: Text(data['time'],
+                            style: GoogleFonts.roboto(
+                                color: Colors.black,
+                                fontWeight: FontWeight.w500)))
+                  ],
+                ),
+              )
+          ],
+        ),
+      ),
+    );
   }
 }
